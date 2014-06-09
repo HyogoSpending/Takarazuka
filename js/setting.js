ceb592b656991e7dab8bb77db78d2e647866eb32
HyogoSpending <HyogoSpending@gmail.com>
2014-06-09 17:58:49 +0900

    up

---
+++ js/setting.js
@@ -0,0 +1,46 @@
+/*
+ * Icons for COFOG taxonomy
+ *
+ */
+var Taxes = Taxes || {};
+
+Taxes.baseKoujo = 330000; // 住民税基礎控除
+Taxes.huyoKoujo = 330000; // 一人分の扶養控除
+Taxes.taxRate = 0.06; // 住民税率
+
+var OpenSpending = OpenSpending || {};
+
+OpenSpending.identifier = 'takarazuka_2012_kessan';//takarazuka_2012_kessan
+OpenSpending.year = '2012';
+
+OpenSpending.Styles = OpenSpending.Styles || {};
+
+OpenSpending.Styles.Cofog = {
+
+  /* yokohama cofog */
+  '20120100': { icon: 'icons/legislative.svg', color: '#705DA8', bcolor: '#935B3B' },
+  '20120101': { icon: 'icons/legislative.svg', color: '#705DA8', bcolor: '#935B3B' },
+  '20120200': { icon: 'icons/publicaffairs.svg', color: '#705DA8', bcolor: '#0AB971' },
+  '20120201': { icon: 'icons/publicaffairs.svg', color: '#705DA8', bcolor: '#0AB971' },
+  '20120300': { icon: 'icons/helping-others.svg', color: '#705DA8', bcolor: '#4E6D00' },
+  '20120301': { icon: 'icons/helping-others.svg', color: '#705DA8', bcolor: '#4E6D00' },
+  '20120400': { icon: 'icons/health.svg', color: '#705DA8', bcolor: '#D33673' },
+  '20120401': { icon: 'icons/health.svg', color: '#705DA8', bcolor: '#D33673' },
+  '20120500': { icon: 'icons/labour.svg', color: '#705DA8', bcolor: '#2A3A03' },
+  '20120501': { icon: 'icons/labour.svg', color: '#705DA8', bcolor: '#2A3A03'  },
+  '20120600': { icon: 'icons/farms.svg', color: '#705DA8', bcolor: '#EC2406' },
+  '20120601': { icon: 'icons/farms.svg', color: '#705DA8', bcolor: '#EC2406' },
+  '20120700': { icon: 'icons/economy-tourism.svg', color: '#705DA8', bcolor: '#938626' },
+  '20120701': { icon: 'icons/economy-tourism.svg', color: '#705DA8', bcolor: '#938626' },
+  '20120800': { icon: 'icons/construction.svg', color: '#705DA8', bcolor: '#705DA8' },
+  '20120801': { icon: 'icons/construction.svg', color: '#705DA8', bcolor: '#705DA8' },
+  '20120900': { icon: 'icons/fire-brigade.svg', color: '#705DA8', bcolor: '#D33673' },
+  '20120901': { icon: 'icons/fire-brigade.svg', color: '#705DA8', bcolor: '#D33673' },
+  '20121000': { icon: 'icons/education.svg', color: '#705DA8', bcolor: '#790586' },
+  '20121001': { icon: 'icons/education.svg', color: '#705DA8', bcolor: '#790586' },
+  '20121100': { icon: 'icons/public-debt.svg', color: '#705DA8', bcolor: '#790586' },
+  '20121101': { icon: 'icons/public-debt.svg', color: '#705DA8', bcolor: '#790586' },
+  '20121200': { icon: 'icons/money.svg', color: '#705DA8', bcolor: '#790586' },
+  '20121201': { icon: 'icons/money.svg', color: '#705DA8', bcolor: '#790586' }
+};
+
